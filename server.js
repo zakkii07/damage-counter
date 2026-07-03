9f30540924ea8b86e6eeb5a908066835e3c91a68
zakkii07 <zakkii0707@gmail.com>
2026-07-03 17:11:06 +0900

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const http = require("http");
 const { Server } = require("socket.io");
-const { exec } = require("child_process");
 
 const app = express();
 const server = http.createServer(app);
@@ -12,8 +11,6 @@ app.use(express.static("public"));
 let counterData = {
   remainingDamage: 100000,
 };
-
-let ngrokStarted = false;
 
 io.on("connection", (socket) => {
   socket.emit("updateCounter", counterData);
@@ -34,36 +31,8 @@ io.on("connection", (socket) => {
   });
 });
 
-app.get("/start-ngrok", (req, res) => {
-  if (!ngrokStarted) {
-    exec(".\\ngrok.exe http 3000");
-    ngrokStarted = true;
-  }
-
-  res.json({ message: "ngrok起動しました" });
-});
-
-app.get("/ngrok-url", async (req, res) => {
-  try {
-    const response = await fetch("http://127.0.0.1:4040/api/tunnels");
-    const data = await response.json();
-
-    const tunnel = data.tunnels.find(t => t.public_url.startsWith("https://"));
-
-    if (!tunnel) {
-      return res.json({ url: "" });
-    }
-
-    res.json({
-      url: tunnel.public_url + "/overlay.html"
-    });
-  } catch (error) {
-    res.json({ url: "" });
-  }
-});
-
 const PORT = process.env.PORT || 3000;
 
 server.listen(PORT, () => {
-    console.log(`サーバー起動：http://localhost:${PORT}`);
+  console.log(`サーバー起動：http://localhost:${PORT}`);
 });
